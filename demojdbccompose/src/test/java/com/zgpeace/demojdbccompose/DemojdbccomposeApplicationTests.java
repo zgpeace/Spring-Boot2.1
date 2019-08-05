36bce812a1cddf4d49808d45631473326dbaa625
zgpeace <zgpeace@gmail.com>
2019-08-05 08:18:30 +0800

    jdbc compose

---
+++ demojdbccompose/src/test/java/com/zgpeace/demojdbccompose/DemojdbccomposeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zgpeace.demojdbccompose;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemojdbccomposeApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
