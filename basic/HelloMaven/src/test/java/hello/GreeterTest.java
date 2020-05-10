eb3cafdb0ff14975b01d7e53a78ae81e1f736dc7
zgpeace <zgpeace@gmail.com>
2020-05-10 19:24:41 +0800

    maven

---
+++ basic/HelloMaven/src/test/java/hello/GreeterTest.java
@@ -0,0 +1,16 @@
+package hello;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GreeterTest {
+
+  private Greeter greeter = new Greeter();
+
+  @Test
+  public void greeterSaysHello() {
+    assertThat(greeter.sayHello(), containsString("Hello"));
+  }
+}
